62341fd7bf498aaf4e98b2037bf143c7433b347a
quiquelowbeat <kikeebcn@gmail.com>
2022-04-26 11:51:11 +0200

    first commit

---
+++ S05T01N01GognomsNom/src/test/java/S05T01N01GognomsNom/ApplicationTests.java
@@ -0,0 +1,13 @@
+package S05T01N01GognomsNom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
