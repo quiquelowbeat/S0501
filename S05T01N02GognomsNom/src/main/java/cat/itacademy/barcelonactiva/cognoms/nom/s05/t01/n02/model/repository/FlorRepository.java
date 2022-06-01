7d88be456fec85e91bc2a6f2959d2e4cc7373f39
quiquelowbeat <kikeebcn@gmail.com>
2022-06-01 17:41:10 +0200

    renamed main folder and refactor

--- S05T01N02GognomsNom/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s05/t01/n02/model/repository/FlorRepository.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.cognoms.nom.s05.t01.n02.model.repository;
-
-import cat.itacademy.barcelonactiva.cognoms.nom.s05.t01.n02.model.domain.FlorEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FlorRepository extends JpaRepository<FlorEntity, Integer> {
-}
